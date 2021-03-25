00bd73a196430e82d07e37cf1d539b946b80dc0a
Suyeon Kang <tndusrkd92@gmail.com>
2021-03-25 22:49:25 +0900

    Generate token for login

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -10,6 +11,7 @@ const UserSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     validate(value) {
@@ -24,7 +26,42 @@ const UserSchema = new mongoose.Schema({
     trim: true,
     minLength: 7,
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
+
+UserSchema.methods.generateAuthToken = async function () {
+  // methods: Instance method
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse');
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
+
+UserSchema.statics.findByCredentials = async (email, password) => {
+  // statics:  Model method
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
+
+  return user;
+};
 
 // Middleware
 UserSchema.pre('save', async function (next) {
@@ -33,9 +70,11 @@ UserSchema.pre('save', async function (next) {
   if (user.isModified('password')) {
     user.password = await bcrypt.hash(user.password, 8);
   }
+
   next();
 });
 
 const User = mongoose.model('User', UserSchema);
+User.createIndexes();
 
 module.exports = User;
--- src/routers/user.js
+++ src/routers/user.js
@@ -6,9 +6,24 @@ router.post('/users', async (req, res) => {
   const user = new User(req.body); // Get from request on frontend
   try {
     await user.save();
+    const token = await user.generateAuthToken();
+    await res.send({ user, token });
     await res.status(201).send(user);
   } catch (err) {
     res.status(400).send(err);
+  }
+});
+
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send();
   }
 });
 
