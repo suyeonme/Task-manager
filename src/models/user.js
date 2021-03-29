9f8f0dd32a98ce16e8d05320a574f7120496977f
Suyeon Kang <tndusrkd92@gmail.com>
2021-03-29 23:09:15 +0900

    Remove tasks when user is removed

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -81,13 +82,19 @@ UserSchema.statics.findByCredentials = async (email, password) => {
 };
 
 // Middleware
+// Hash password before saving
 UserSchema.pre('save', async function (next) {
   const user = this;
-
   if (user.isModified('password')) {
     user.password = await bcrypt.hash(user.password, 8);
   }
+  next();
+});
 
+// Remove tasks when user is removed
+UserSchema.pre('remove', async function (next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
   next();
 });
 
