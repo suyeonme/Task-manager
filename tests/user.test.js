599d2866d695371e6c21bf76c227501f7a7f3add
Suyeon Kang <tndusrkd92@gmail.com>
2021-11-20 20:46:19 +0900

    testing for an image

--- tests/user.test.js
+++ tests/user.test.js
@@ -96,6 +96,36 @@ test('Should not delete account', async () => {
     .expect(401);
 });
 
+test('Should upload avatar image', async () => {
+  await request(app)
+    .post('/users/me/avatar')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .attach('avatar', 'tests/fixtures/test-image.png')
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test('Should update valid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({ name: 'James' })
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user.name).toBe('James');
+});
+
+test('Should not update invalid valid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({ age: 26 })
+    .expect(400);
+});
+
 // Authentication
 test('Should get profile for user', async () => {
   await request(app)
